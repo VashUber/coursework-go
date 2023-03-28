b44bbbec3e8a75c7792bfd890296072ccb8e32a4
VashUber <71492611+VashUber@users.noreply.github.com>
2023-03-28 20:59:57 +0300

    feature: added client side

---
+++ client/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ $t("hello") }}
+
+    <button @click="changeLocale">{{ $i18n.global.locale }}</button>
+  </div>
+</template>
+
+<script setup>
+const { $i18n } = useNuxtApp();
+
+const lang = $i18n.global.locale;
+
+const changeLocale = () => {
+  if (lang.value === "en") {
+    $i18n.global.locale.value = "ru";
+    return;
+  }
+
+  $i18n.global.locale.value = "en";
+};
+</script>
